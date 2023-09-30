9397eaa972b0b5d4adc09b7f5b8f9d54bf830bd6
rahulkumar <rahulkumar@yourdost.com>
2023-09-30 13:02:44 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFf9f9f9),
+      appBar: AppBar(
+        backgroundColor: Color(0xffF0ECFF),
+        title: const Text('Weather App',style: TextStyle(
+          fontWeight: FontWeight.bold,
+          // fontFamily: 'Open Sans'
+        ),),
+        centerTitle: true,
+        leading: IconButton(onPressed: (){},icon:const Icon(Icons.menu),),
+      ),
+    );
+  }
+}
+
+
